410ec597a6f1e31a7bcff8c2d852ea2d90f11bde
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-11-02 00:31:31 +0800

    popup styles

--- popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggleStyles').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.tabs.sendMessage(tab.id, { action: 'toggleStyles' });
-});
