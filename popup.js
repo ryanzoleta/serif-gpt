656fbf5acb93a134e13fec8cf287ccb2f8e2f5af
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-11-01 22:27:33 +0800

    initial commit

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleStyles').addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.tabs.sendMessage(tab.id, { action: 'toggleStyles' });
+});
